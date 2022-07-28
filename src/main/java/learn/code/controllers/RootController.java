e26a91deded490f3a0651db08f10b33517d395c9
Nick <koli.1997@gmail.com>
2022-07-28 15:38:44 +0300

    Добавлена страница urls

--- src/main/java/learn/code/controllers/RootController.java
+++ src/main/java/learn/code/controllers/RootController.java
@@ -5,8 +5,5 @@ import io.javalin.http.Handler;
 public final class RootController {
 
     public static Handler welcome = ctx -> ctx.render("index.html");
-
-//    public static Handler about = ctx -> {
-//        ctx.render("about.html");
-//    };
+    public static Handler urls = ctx -> ctx.render("urls.html");
 }
